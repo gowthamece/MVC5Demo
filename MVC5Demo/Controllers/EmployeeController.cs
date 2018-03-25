2c5860125453007e2e02258a3c26af0ddeed6756
Gowtham K <gowthamk91@outlook.com>
2018-03-25 21:57:14 +0530

    Add project files.

---
+++ MVC5Demo/Controllers/EmployeeController.cs
@@ -0,0 +1,36 @@
+﻿using MVC5Demo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Demo.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            Employee employee = new Employee();
+            employee.FirstName = "Gowtham";
+            employee.LastName = "K";
+            employee.Age = "26";
+            return View("EmployeeIndex",employee);
+        }
+
+        [HttpPost]
+        public ActionResult AddEmployee(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("AddEmployee_Success");
+            }
+            else
+            {
+                return View("EmployeeIndex");
+            }
+        }
+       
+    }
+}
---
+++ MVC5Demo/Models/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC5Demo.Models
+{
+    public class Employee
+    {
+        [Required(ErrorMessage ="Required")]
+        public string  FirstName { get; set; }
+        public string  LastName { get; set; }
+        [Required(ErrorMessage ="Required")]
+        [EmailAddress(ErrorMessage ="Enter Valid Email Addredd")]
+        public string Email { get; set; }
+        [Required(ErrorMessage ="Required")]
+        public string Password { get; set; }
+        [Required(ErrorMessage ="Required")]
+        [Compare("Password",ErrorMessage ="Password doesn't match")]
+        public string ConfirmPassword { get; set; }
+        public string Age { get; set; }
+    }
+}
